77aa37e454cae44cbb4df171a4c2ed92472b265e
YoungBeomLee <edlee127127@gmail.com>
2023-03-26 18:37:58 +0900

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../UI/Container";
+import styles from "../css/header.module.css";
+import logoImg from "../img/Logo.png"
+
+
+const Header = () => {
+    return (
+      <header>
+        <Container>
+        <div className={styles.gnbWrap}>
+          <div className={styles.logoWrap}>
+            <Link to='/'>
+              <h1>
+                <img src={logoImg} alt='logo' />
+              </h1>
+            </Link>
+            </div>
+            <div className={styles.topMenu}>
+          
+              <ul className={styles.menu}>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/'>About me</Link>
+                </li>
+                <li>
+                  <Link to='/'>PortFolio</Link>
+                </li>
+                <li>
+                  <Link to='/'>Skils</Link>
+                </li>
+                <li>
+                  <Link to='/'>Contact Me</Link>
+                </li>
+                <li>
+                  <Link to='/'>택배조회</Link>
+                </li>
+              </ul>
+          
+          </div>
+               
+        </div>
+      
+      </Container>
+        
+        
+    </header>
+
+        );
+};
+
+export default Header;
