0b299f3713b968c3fb589680e6f00014440e9950
YoungBeomLee <edlee127127@gmail.com>
2023-03-29 00:25:10 +0900

    23-03-29 [이영범] 메인배너 바꾸기

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,6 +1,8 @@
 import React from "react";
 import BannerContainer from "../UI/BannerContainer";
 import styles from "../css/Banner.module.css";
+import circle from "../img/circle.png";
+import {FaGithub} from "react-icons/fa";
 
 
 const Header = () => {
@@ -9,15 +11,25 @@ const Header = () => {
       
       <BannerContainer>
           <div className={styles.bannerWrap}>
+            <div className={styles.circleOne}><img src={circle} alt=""/></div>
             <div class={styles.innerWrap}>
                 <div className={styles.bannerText}>
                     <div className={styles.bannerName}>
                         <h2><span>YoungBeom Lee</span></h2>
                     </div>
                     <p className={styles.bannerDesc}><span>신입 프론트엔드개발자 이영범입니다.</span></p>
-                    <a href="#">Read Me</a>
+                    <h4>"Specialized in" &nbsp;
+                      <span>Freelancer.</span>
+                      <span></span>
+                    </h4>
+                    <a href="https://github.com/YoungBeomLee" target="_blank" >
+                    <div className={styles.gitHub}>
+                     <FaGithub className={styles.faGithub}/>
+                      <span>GitHub</span>
+                    </div>
+                    </a>
                 </div>
-                <div className={styles.bannerImgbox}></div>
+                
             </div>
           </div>
       </BannerContainer>
