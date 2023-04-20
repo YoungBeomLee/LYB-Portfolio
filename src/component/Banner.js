c97cdd5ce5a55a19da1f32d37ec7020260d5f332
YoungBeomLee <edlee127127@gmail.com>
2023-04-20 18:20:02 +0900

    23-04-20 banner inner 작업 수정

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef, useEffect } from "react";
+import { useLayoutEffect, useRef, useEffect, useState } from "react";
 import BannerContainer from "../UI/BannerContainer";
 import styles from "../css/Banner.module.css";
 import circle from "../img/circle.png";
@@ -9,22 +9,30 @@ import { TextPlugin } from "gsap/TextPlugin";
 import { AiFillSetting } from "react-icons/ai";
 import profile from "../img/profile3.png";
 
-
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 const Banner = () => {
   const firstRef = useRef(null);
   const secondRef = useRef(null);
+  const thirdRef = useRef(null);
+  const fourthRef = useRef(null);
+  const [typeItem, setTypeItem] = useState(() => {});
 
   useEffect(() => {
     const first = firstRef.current;
     const second = secondRef.current;
+    const third = thirdRef.current;
+    const fourth = fourthRef.current;
 
     const tl = gsap.timeline({ repeat: -1 });
 
-    tl.to(first, { duration: 5.5, text: "이번네트워크는 영범포트입니다. 내리실 문은 프론트입니다. ", delimiter: " ", split: "chars", ease: "Linear.easeNone" })
-      .to(first, { duration: 3, text: "", split: "", ease: "Linear.easeNone" })
-      .to(second, { duration: 5.5, text: "This Network is Youngbeom Port Station. The doors are on  Frontfolio. ", split: "chars", ease: "Linear.easeNone" })
-      .to(second, { duration: 3, text: "", split: "", ease: "Linear.easeNone" });
+    tl.to(first, { duration: 5.5, text: "이번 네트워크는 영범포트입니다.  ", delimiter: " ", split: "chars", ease: "Linear.easeNone", opacity: 1 })
+      .to(first, { duration: 0.1, opacity: 0, text: "" })
+      .to(second, { duration: 5.5, text: "내리실 문은 프론트입니다.", split: "chars", ease: "Linear.easeNone" })
+      .to(second, { duration: 0.1, opacity: 0, text: "" })
+      .to(third, { duration: 5.5, text: "This Network is Youngbeom Port Station. ", split: "chars", ease: "Linear.easeNone" })
+      .to(third, { duration: 0.1, opacity: 0, text: "" })
+      .to(fourth, { duration: 5.5, text: " The doors are on  Frontfolio. ", split: "chars", ease: "Linear.easeNone" })
+      .to(fourth, { duration: 0.1, opacity: 0, text: "" });
   }, []);
 
   return (
@@ -39,7 +47,8 @@ const Banner = () => {
               <div className={styles.bannerText}>
                 <div className={styles.bannerName}>
                   <h2>
-                    <span>YoungBeom's</span><br/>
+                    <span>YoungBeom's</span>
+                    <br />
                     <span>Portfolio</span>
                   </h2>
                 </div>
@@ -48,14 +57,18 @@ const Banner = () => {
                 </p>
                 <h4>
                   "영범포트" &nbsp;
+                  <AiFillSetting className={styles.setting}></AiFillSetting>
+                </h4>
+                <h4 className={styles.aniBox}>
                   <span className={styles.typeCss} ref={firstRef}></span>
                   <span className={styles.typeCss2} ref={secondRef}></span>
-                  <AiFillSetting className={styles.setting}></AiFillSetting>
+                  <span className={styles.typeCss2} ref={thirdRef}></span>
+                  <span className={styles.typeCss2} ref={fourthRef}></span>
                 </h4>
                 <ul className={styles.iconList}>
                   <li>
                     <a href="https://github.com/YoungBeomLee">
-                      <FaGithub className={styles.faGithub} />
+                      <span>깃허브</span>
                     </a>
                   </li>
                   <li>
@@ -66,7 +79,9 @@ const Banner = () => {
                 </ul>
               </div>
             </div>
-            <div className={styles.ImageBox}><img src={profile}/></div> 
+            <div className={styles.ImageBox}>
+              <img src={profile} />
+            </div>
           </div>
         </div>
       </BannerContainer>
